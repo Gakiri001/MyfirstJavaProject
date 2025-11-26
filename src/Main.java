ab71ab435e2e5f6a583a5854ce8ef0977f47ab55
Gakiri001 <gakirijulius@gmail.com>
2025-11-26 12:32:18 +0300

    My first java project

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public  static void main(String[] args){
+         System.out.println("I love java");
+         System.out.print("Kenya is a beautiful country\n");
+         System.out.print("You need to visit kenya - Maasai Mara National Park\n");
+         //comment - this is my first java project
+        /*
+        This
+        is
+        multi-line
+        comment
+         */
+        int age=22;
+        System.out.println("My Age is "+age);
+
+        int year = 2025;
+        System.out.println("The year is "+year);
+
+        int quantity = 676;
+        System.out.println("The quantity is "+quantity);
+
+        double price = 12;
+        System.out.println("$" + price);
+
+        double temp = -88.6;
+        System.out.println("The Temperature is " + temp + " ᰱᰱᰱᰱᰱC");
+
+        char grade = 'A';
+        System.out.println(grade);
+
+        char symbol = '!';
+        System.out.println(symbol);
+
+        char currency = '$';
+        System.out.println(currency);
+
+        boolean isStudent = false;
+        System.out.println(isStudent);
+
+        boolean forSale = false;
+        System.out.println(forSale);
+
+        if(isStudent == true){
+            System.out.println("You are a student");
+        }
+        else {
+            System.out.println("You aren't a student");
+        }
+    }
+}
