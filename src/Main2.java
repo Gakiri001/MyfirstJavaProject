b59f5e3df257fc68fc011899f01f4e08b2eb3a4e
Gakiri001 <gakirijulius@gmail.com>
2025-11-26 14:00:56 +0300

    Input request 2

--- src/Main2.java
+++ src/Main2.java
@@ -4,7 +4,7 @@ public class Main2 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your name");
+        System.out.println("Enter your namee?");
 
         scanner.close();
     }
